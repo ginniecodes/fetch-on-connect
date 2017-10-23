c3670a436c499d694cc0690d9ed2177d391a57c1
Jhinel Arcaya <proyectoamicoro@gmail.com>
2017-10-23 13:02:49 -0400

    usage in README

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # fetch-on-connect
 Requests in react-native when connection is available
+
+## Usage
+
+```
+import fetch from 'fetch-on-connect';
+
+fetch('https://randomapi.com/api/6de6abfedb24f889e0b5f675edc50deb?fmt=raw&sole')
+  .then((res) => { /* do your magic */ });
+
+```
+
--- index.js
+++ index.js
@@ -4,14 +4,14 @@ export default function fetchOnConnection(url, args) {
   return NetInfo.isConnected.fetch().then(connected => {
     if(connected) {
       try {
-        NetInfo.isConnected.removeEventListener('connectionChange', fetchOnConnection.bind(undefined, funct, ...args))
+        NetInfo.isConnected.removeEventListener('connectionChange', fetchOnConnection.bind(undefined, url, args))
         return fetch(url, args)
       } catch(e) {
         throw e
       }
     }
     else {
-      NetInfo.isConnected.addEventListener('connectionChange', fetchOnConnection.bind(undefined, funct, ...args))
+      NetInfo.isConnected.addEventListener('connectionChange', fetchOnConnection.bind(undefined, url, args))
     }
   })
 }
